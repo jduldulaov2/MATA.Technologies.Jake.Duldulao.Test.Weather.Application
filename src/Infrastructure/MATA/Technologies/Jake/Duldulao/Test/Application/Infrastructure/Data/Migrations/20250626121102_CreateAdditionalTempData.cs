5c9bbb4288578a61ed6c44861e053369d63585d6
jake duldulao <jake.duldulao@datasoftlogic.com>
2025-06-26 05:34:13 -0700

    Backend: Add new migration for temporary data on Forecast. Modify some CSS related on dashboard.

---
+++ src/Infrastructure/MATA/Technologies/Jake/Duldulao/Test/Application/Infrastructure/Data/Migrations/20250626121102_CreateAdditionalTempData.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MATA.Technologies.Jake.Duldulao.Test.Application.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateAdditionalTempData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+             INSERT INTO Forecast (
+                 City, ForecastName, ForecastDescription, ForecastDate, ForecastMain, 
+                 Pressure, Humidity, Temperature,
+                 UniqueId, Created, CreatedBy, LastModified
+                 ) VALUES
+                 ('Nashville', 'Humid Morning', 'High humidity with a chance of showers', '2025-06-28', 'Humidity', '1012', '85', '82.5', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000021', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('Indianapolis', 'Partly Sunny', 'Clouds early, clearing by noon', '2025-06-28', 'Clouds', '1011', '50', '79.8', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000022', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('Columbus', 'Clear', 'Clear skies and sunny', '2025-06-28', 'Clear', '1013', '45', '80.1', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000023', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('Baltimore', 'Showers', 'Light rain throughout the day', '2025-06-28', 'Rain', '1009', '70', '67.2', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000024', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('Salt Lake City', 'Sunny', 'Hot and sunny all day', '2025-06-28', 'Clear', '1015', '20', '96.3', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000025', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('Kansas City', 'Rainstorm', 'Heavy rain in the morning', '2025-06-28', 'Storm', '1005', '90', '73.0', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000026', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('Cincinnati', 'Cool Breeze', 'Cool morning with breeze', '2025-06-28', 'Wind', '1020', '40', '68.9', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000027', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('Cleveland', 'Overcast Skies', 'Cloudy and mild', '2025-06-28', 'Clouds', '1012', '60', '70.6', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000028', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('Pittsburgh', 'Thunderstorm Alert', 'Scattered thunderstorms', '2025-06-28', 'Storm', '1006', '80', '74.3', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000029', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('St. Louis', 'Hot Afternoon', 'Hot temperatures in the afternoon', '2025-06-28', 'Hot', '1011', '65', '91.4', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000030', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('Milwaukee', 'Chilly Winds', 'Cool and breezy day', '2025-06-28', 'Wind', '1021', '30', '60.1', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000031', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('Tampa', 'Humid Heat', 'Hot with high humidity', '2025-06-28', 'Hot', '1010', '88', '89.9', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000032', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('New Orleans', 'Stormy Weather', 'Heavy rain and lightning', '2025-06-28', 'Storm', '1007', '92', '85.7', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000033', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('Oklahoma City', 'Dry Thunder', 'Thunderstorms with little rain', '2025-06-28', 'Storm', '1008', '50', '83.3', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000034', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('Albuquerque', 'Clear and Hot', 'Dry, sunny, and hot', '2025-06-28', 'Clear', '1006', '25', '101.2', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000035', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('Raleigh', 'Mild and Cloudy', 'Overcast with light breeze', '2025-06-28', 'Clouds', '1014', '55', '76.0', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000036', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('Richmond', 'Morning Mist', 'Light fog clearing by mid-morning', '2025-06-28', 'Fog', '1012', '68', '71.4', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000037', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('Boise', 'Warm and Dry', 'Dry and sunny conditions', '2025-06-28', 'Clear', '1016', '28', '88.0', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000038', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('Honolulu', 'Tropical Sun', 'Hot and sunny with breeze', '2025-06-28', 'Clear', '1010', '60', '86.6', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000039', '2025-06-28', 'SystemSeeder', '2025-06-28'),
+                 ('Anchorage', 'Cool and Clear', 'Cool with sunshine', '2025-06-28', 'Clear', '1023', '40', '58.7', 
+                  'd1a7b760-e1b4-41c7-a6e1-000000000040', '2025-06-28', 'SystemSeeder', '2025-06-28');
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DELETE FROM Forecast
+                WHERE CreatedBy = 'SystemSeeder' AND Created = '2025-06-28';
+            ");
+        }
+    }
+}
