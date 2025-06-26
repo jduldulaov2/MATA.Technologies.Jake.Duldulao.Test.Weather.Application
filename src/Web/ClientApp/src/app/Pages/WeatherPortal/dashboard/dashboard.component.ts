ca292914a7460049f114b93ccf3f52690f652a85
jake duldulao <jake.duldulao@datasoftlogic.com>
2025-06-26 04:40:21 -0700

    Front End: Implement the HTML layout and behind code (typescript) for the Dashboard

--- src/Web/ClientApp/src/app/Pages/WeatherPortal/dashboard/dashboard.component.ts
+++ src/Web/ClientApp/src/app/Pages/WeatherPortal/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ForecastsClient, GetAllForecastQueryDto } from '../../../web-api-client';
 import { SpinnerServiceService } from '../../../Services/Shared/spinner-service.service';
 declare var $: any;
 
@@ -8,6 +9,29 @@ declare var $: any;
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
 export class DashboardComponent {
+
+  public forcastDto : GetAllForecastQueryDto[] = [];
+
+  constructor(
+    private forecastClient: ForecastsClient,
+    private loader: SpinnerServiceService
+  ) { }
+
+  ngOnInit(){
+    this.loader.ShowLoader();
+    this.getForecastList();
+  }
+
+  getForecastList(): void {
+    this.forecastClient.getAllForcast().subscribe({
+      next: result => {
+        this.forcastDto = result
+        console.log(result);
+      },
+      error: error => console.error(error)
+    });
+  }
 
 }
